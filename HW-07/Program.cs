a910626e4475abee9aee058d5f00d978c2aa314d
Artur <artur071982@gmail.com>
2021-03-16 15:12:01 +0500

    Initial commit

---
+++ HW-07/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secret = "sos";
+            Console.WriteLine("Enter password:");
+            string input = Console.ReadLine();
+            if (input == secret)
+            {
+                Console.WriteLine("Welcome!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
